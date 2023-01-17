fb9255c37eb58924764887ffce21a161ce4bf7f3
Alexandre Laira <alexandrelaira2@gmail.com>
2023-01-17 09:07:54 -0300

    Adicionado Javascript

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+
+const nome = document.querySelector('.apresentacao h1');
+const popup = document.querySelector('.popup');
+
+function abrePopup(event){
+  popup.classList.toggle('ativo');
+}
+
+
+nome.addEventListener('click', abrePopup);
