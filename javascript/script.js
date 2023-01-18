19c691b130551dd7216101114d7323e4a79bda1b
Alexandre Laira <alexandrelaira2@gmail.com>
2023-01-17 22:36:10 -0300

    Atualizacao js sobre

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,14 @@
 
 const nome = document.querySelector('.apresentacao h1');
 const popup = document.querySelector('.popup');
+const popupH4 = document.querySelector('.popup h4');
+const popupP = document.querySelector('.popup p');
 
 function abrePopup(event){
   popup.classList.toggle('ativo');
+  popupH1.classList.toggle('ativo');
+  popupP.classList.toggle('ativo');
+
 }
 
 
