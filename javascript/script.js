c952b5f4b7bae6219548cf579647920c4f09f8da
Alexandre Laira <alexandrelaira2@gmail.com>
2023-01-25 21:45:32 -0300

    Ajustando design responsivo

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,35 @@
+function popUp(){
+  const nome = document.querySelector('.apresentacao h1');
+  const popup = document.querySelector('.popup');
+  const popupH4 = document.querySelector('.popup h4');
+  const popupP = document.querySelector('.popup p');
 
-const nome = document.querySelector('.apresentacao h1');
-const popup = document.querySelector('.popup');
-const popupH4 = document.querySelector('.popup h4');
-const popupP = document.querySelector('.popup p');
+  const sobre = document.querySelector('.links');
 
-const sobre = document.querySelector('.links');
+  function abrePopup(event){
+    popup.classList.toggle('ativo');
+    popupH1.classList.toggle('ativo');
+    popupP.classList.toggle('ativo');
 
-function abrePopup(event){
-  popup.classList.toggle('ativo');
-  popupH1.classList.toggle('ativo');
-  popupP.classList.toggle('ativo');
+  }
+
+
+  nome.addEventListener('click', abrePopup);
+  sobre.addEventListener('click', abrePopup);
+
+}
+popUp();
+
+
+const menu = document.querySelector('.header span');
+let lista = document.querySelector('.lista-header');
+
+function abreDisplay(event){
+
+  lista.classList.toggle('ativo');
 
 }
 
-
-nome.addEventListener('click', abrePopup);
-sobre.addEventListener('click', abrePopup);
-
-
+menu.addEventListener('click', abreDisplay);
 
 
