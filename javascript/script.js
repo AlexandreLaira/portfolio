e9fa8b98d2f1bfcebde41d7dea67bb7fbf275599
Alexandre Laira <alexandrelaira2@gmail.com>
2023-03-02 21:39:21 -0300

    Atualizacao geral

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,24 @@
 function popUp(){
+
+  const fundo = document.getElementById('fundo');
   const nome = document.querySelector('.apresentacao h1');
   const popup = document.querySelector('.popup');
-  const popupH4 = document.querySelector('.popup h4');
-  const popupP = document.querySelector('.popup p');
-
+  // const popupP = document.querySelector('.popup p');
   const sobre = document.querySelector('.links');
+  const botaoPop = document.querySelector('.botao-popup');
 
   function abrePopup(event){
     popup.classList.toggle('ativo');
-    popupH1.classList.toggle('ativo');
-    popupP.classList.toggle('ativo');
+    // popupH1.classList.toggle('ativo');
+    // popupP.classList.toggle('ativo');
+    fundo.classList.toggle('fundo');
 
   }
 
 
   nome.addEventListener('click', abrePopup);
   sobre.addEventListener('click', abrePopup);
+  botaoPop.addEventListener('click', abrePopup);
 
 }
 popUp();
@@ -33,3 +36,8 @@ function abreDisplay(event){
 menu.addEventListener('click', abreDisplay);
 
 
+let descExperiencia = document.querySelectorAll('.empresa-experiencia').forEach((event) => {
+  event.addEventListener('click', ()=> {
+    event.classList.toggle('ativo');
+  });
+})
