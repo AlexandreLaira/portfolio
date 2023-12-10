d69cfdd2baba40f76fb0c4d65d55a343dd1bab50
AlexandreLaira <alexandrelaira2@gmail.com>
2023-12-10 08:16:58 -0300

    melhorando desempenho

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,1 @@
-import {asideScript, scrollSide} from "./aside_script.js";
-import { menuLista } from "./menu.js";
-import { scrollSuave } from "./scrollSuave.js";
-import {parallaxMain} from './parallax.js'
-import { habilidadeSlide } from "./habilidadesSlide.js";
-
-habilidadeSlide();
-scrollSide();
-asideScript();
-menuLista();
-scrollSuave();
-parallaxMain();
+import{asideScript,scrollSide}from"./aside_script.js";import{menuLista}from"./menu.js";import{scrollSuave}from"./scrollSuave.js";import{parallaxMain}from"./parallax.js";import{habilidadeSlide}from"./habilidadesSlide.js";habilidadeSlide(),scrollSide(),asideScript(),menuLista(),scrollSuave(),parallaxMain();
