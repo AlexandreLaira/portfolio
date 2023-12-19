17f704db1371149a7eb9914fdee83a4865441f4c
AlexandreLaira <alexandrelaira2@gmail.com>
2023-12-19 15:10:23 -0300

    mudanca layout menu mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,13 @@
-function menuLista(){const botaoMenu=document.querySelector("[data-botao]"),menuLista=document.querySelector("[data-menu]");function ativaMenu(event){menuLista.classList.toggle("ativa-menu"),botaoMenu.classList.toggle("viraBotaoMenu")}botaoMenu.addEventListener("click",ativaMenu);const listaMobile=undefined;document.querySelectorAll("li[data-listaMobile]").forEach(item=>{item.addEventListener("click",ativaMenu)})}export{menuLista};
+function menuLista() {
+  const botaoMenu = document.querySelector("[data-botao]"),
+        menuLista = document.querySelector("[data-menu]");
+  function ativaMenu(event) {
+      menuLista.classList.toggle("ativa-menu"), botaoMenu.classList.toggle("viraBotaoMenu");
+  }
+  botaoMenu.addEventListener("click", ativaMenu);
+  const listaMobile = undefined;
+  document.querySelectorAll("li[data-listaMobile]").forEach((item) => {
+      item.addEventListener("click", ativaMenu);
+  });
+}
+export { menuLista };
