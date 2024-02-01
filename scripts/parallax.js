fc9519da05a137c82bce77df294ba8600f0ff9c6
AlexandreLaira <alexandrelaira2@gmail.com>
2024-02-01 08:37:47 -0300

    Ajuste Acessibilidade

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,54 +1,45 @@
-
-export function parallaxMain(){
-const titulocentral = document.querySelector(".principal-titulo h1");
-const paragrafoCentral = document.querySelector(".principal-titulo h2");
-const principalTitulo = document.querySelector(".principal-titulo");
-const botaoIncial = document.querySelector("#botao");
-const nome = document.querySelector(".principal p");
-const menu = document.querySelector(".principal ul");
-function moveUp(e) {
-  const topDistance = e.currentTarget.scrollY;
-  const alturaTelaIncial = principalTitulo.clientHeight;
-  titulocentral.style.top = -0.73 * topDistance + "px";
-  titulocentral.style.opacity = 1 - .003 * topDistance;
-  paragrafoCentral.style.top = -1.03 * topDistance + "px";
-  paragrafoCentral.style.opacity = 1 - .003 * topDistance;
-  botaoIncial.style.top = -1.03 * topDistance + "px";
-  botaoIncial.style.opacity = 1 - .003 * topDistance;
-  const imagem = document.querySelector(".imagem img");
-  const conteudo = document.querySelector(".conteudo-sobre");
-  if (topDistance < alturaTelaIncial + 100) {
-    imagem.style.left = .3 * topDistance - 150 + "px";
-    conteudo.style.right = .3 * topDistance - 350 + "px";
-    imagem.style.opacity = 0 + .002 * topDistance;
-    conteudo.style.opacity = 0 + .002 * topDistance;
+export function parallaxMain() {
+  const titulocentral = document.querySelector('.principal-titulo h1');
+  const paragrafoCentral = document.querySelector('.principal-titulo h2');
+  const principalTitulo = document.querySelector('.principal-titulo');
+  const botaoIncial = document.querySelector('#botao');
+  const nome = document.querySelector('.principal p');
+  const menu = document.querySelector('.principal ul');
+  function moveUp(e) {
+    const topDistance = e.currentTarget.scrollY;
+    const alturaTelaIncial = principalTitulo.clientHeight;
+    titulocentral.style.top = -0.73 * topDistance + 'px';
+    titulocentral.style.opacity = 1 - 0.003 * topDistance;
+    paragrafoCentral.style.top = -1.03 * topDistance + 'px';
+    paragrafoCentral.style.opacity = 1 - 0.003 * topDistance;
+    botaoIncial.style.top = -1.03 * topDistance + 'px';
+    botaoIncial.style.opacity = 1 - 0.003 * topDistance;
+    const imagem = document.querySelector('.imagem img');
+    const conteudo = document.querySelector('.conteudo-sobre');
+    if (topDistance < alturaTelaIncial + 100) {
+      imagem.style.left = 0.3 * topDistance - 150 + 'px';
+      conteudo.style.right = 0.3 * topDistance - 350 + 'px';
+      imagem.style.opacity = 0 + 0.002 * topDistance;
+      conteudo.style.opacity = 0 + 0.002 * topDistance;
+    }
   }
-}
-setTimeout(() => {
-  nome.style.opacity = 1;
-  menu.style.opacity = 1;
-  principalTitulo.style.opacity = 1;
-}, 300);
-window.addEventListener("scroll", moveUp);
+  setTimeout(() => {
+    nome.style.opacity = 1;
+    menu.style.opacity = 1;
+    principalTitulo.style.opacity = 1;
+  }, 300);
+  window.addEventListener('scroll', moveUp);
 }
 
-function devTitle(){
+function devTitle() {
   const titulo = document.querySelector('.principal-titulo h1');
-  
-  console.log(titulo);
 
-  function handleMove(e){
+  function handleMove(e) {
     let x = e.clientX;
     let y = e.clientY;
-
-
-    
-
   }
 
-  window.addEventListener('mousemove', handleMove)
-
-
+  window.addEventListener('mousemove', handleMove);
 }
 
 devTitle();
